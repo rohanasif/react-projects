93a55e1ae8a03e1868df24822415cd4ff9fce92e
Rohan <rohanasif1990@gmail.com>
2022-11-10 20:33:08 +0500

    few changes in menu project

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -3,8 +3,29 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ['all', ...new Set(
+  items.map((item) =>
+    item.category
+  ))]
+
+
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items)
+  const [categories, setCategories] = useState(allCategories)
+  return (
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>our menu</h2>
+        </div>
+        <div className='underline'>
+
+        </div>
+        <Categories categories={categories} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  )
 }
 
 export default App;
